stalyatech/u-boot

commit ca00e437aa5787ef691c61ae73db7a3f79976424

stm32mp1: update Android support
Update the Android integration (Android 10 / Android 11 compliant)
and generate the needed variables for FASTBOOT and android bootargs
(OP-TEE detection).

Allow to boot android with CONFIG_BOOTCOMMAND="run bootcmd_android"

This bootcmd override the DISTRO script "mmc_boot" to boot android
using system_${suffix} partition (with "_a") by default
- display splash screen
- load device tree form dtimg
- load kernel and set bootargs
- start kernel

Signed-off-by: Nicolas LOUBOUTIN <nicolas.louboutin@st.com>
Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
Change-Id: I5f23177a9a5f3d5df618876c4d0ed1e3d680ce5a
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/177858
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>

board/st/stm32mp1/stm32mp1.c
@@ -22,6 +22,7 @@
 #include <remoteproc.h>
 #include <reset.h>
 #include <syscon.h>
+#include <tee.h>
 #include <usb.h>
 #include <watchdog.h>
 #include <asm/io.h>
@@ -737,7 +738,7 @@ int board_init(void)
 
 int board_late_init(void)
 {
-	char *boot_device;
+	char *boot_device, *boot_instance;
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	const void *fdt_compat;
 	int fdt_compat_len;
@@ -790,6 +791,25 @@ int board_late_init(void)
 	if (boot_device &&
 	    (!strcmp(boot_device, "serial") || !strcmp(boot_device, "usb")))
 		env_set("bootdelay", "0");
+
+	/* define dynamic variables for FASTBOOT and ANDROID bootargs*/
+	if (CONFIG_IS_ENABLED(CONFIG_FASTBOOT_FLASH_MMC) &&
+	    boot_device && !strcmp(boot_device, "mmc")) {
+		boot_instance = env_get("boot_instance");
+		env_set("fastboot.boot_instance", boot_instance);
+	}
+	if (CONFIG_IS_ENABLED(OPTEE) &&
+	    tee_find_device(NULL, NULL, NULL, NULL)) {
+		if (CONFIG_IS_ENABLED(CONFIG_CMD_DTIMG))
+			env_set("android_bootargs", "androidboot.optee=true");
+		if (CONFIG_IS_ENABLED(FASTBOOT))
+			env_set("fastboot.boot_mode", "optee");
+	} else {
+		if (CONFIG_IS_ENABLED(CONFIG_CMD_DTIMG))
+			env_set("android_bootargs", "");
+		if (CONFIG_IS_ENABLED(FASTBOOT))
+			env_set("fastboot.boot_mode", "trusted");
+	}
 
 	return 0;
 }

include/configs/stm32mp1.h
@@ -168,7 +168,7 @@
 
 #define STM32MP_ANDROID \
 	"suffix=a\0" \
-	"dtimg_addr=0xc4500000\0" \
+	"dtimg_addr=0xc44FFF80\0" \
 	"android_mmc_splash="\
 		"if part start mmc ${devnum} splash splash_start && " \
 		   "part size mmc ${devnum} splash splash_size;"\
@@ -186,22 +186,20 @@
 		   "dtimg start ${dtimg_addr} ${dt_index} fdt_addr_r;"\
 		"fi\0" \
 	"android_mmc_kernel="\
-		"if part start mmc ${devnum} boot_${suffix} boot_start &&" \
+		"if part start mmc ${devnum} boot_${suffix} boot_start && " \
 		   "part size mmc ${devnum} boot_${suffix} boot_size;"\
 		"then " \
 		   "mmc read ${kernel_addr_r} ${boot_start} ${boot_size};" \
-		   "part nb mmc ${devnum} system_${suffix} rootpart_nb;" \
-		   "env set bootargs" \
-		     "root=/dev/mmcblk${devnum}p${rootpart_nb} " \
+		   "env set bootargs ${android_bootargs} " \
 		     "androidboot.serialno=${serial#} " \
-		     "androidboot.slot_suffix=_${suffix};"\
+		     "androidboot.slot_suffix=_${suffix};" \
 		"fi\0" \
 	"android_mmc_boot="\
 		"mmc dev ${devnum};"\
 		"run android_mmc_splash;" \
 		"run android_mmc_fdt;" \
 		"run android_mmc_kernel;" \
-		"bootm ${kernel_addr_r} - ${fdt_addr_r};\0" \
+		"bootm ${kernel_addr_r} ${kernel_addr_r} ${fdt_addr_r};\0" \
 	"bootcmd_android=" \
 		"env set mmc_boot run android_mmc_boot;" \
 		"run bootcmd_stm32mp\0"

commit 8288f5fe3dc2edfe4577a54fb8ed6e22a9fa8124

board: st: stm32mp1:  Fix wrong CONFIG_IS_ENABLED() handling for ANDROID bootargs
CONFIG_IS_ENABLED() takes the kconfig name without the CONFIG_ prefix,
this patch fixes this macro uasage in board_late_init to treat the
ANDROID bootargs.

Fixes: ca00e437aa57 ("stm32mp1: update Android support")
Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
Signed-off-by: Nicolas LOUBOUTIN <nicolas.louboutin@st.com>
Change-Id: I4ea34955ea03ef90e5b67799308c5931b7b2ba00
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/184346
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>

board/st/stm32mp1/stm32mp1.c
@@ -812,19 +812,19 @@ int board_late_init(void)
 		env_set("bootdelay", "0");
 
 	/* define dynamic variables for FASTBOOT and ANDROID bootargs*/
-	if (CONFIG_IS_ENABLED(CONFIG_FASTBOOT_FLASH_MMC) &&
+	if (CONFIG_IS_ENABLED(FASTBOOT_FLASH_MMC) &&
 	    boot_device && !strcmp(boot_device, "mmc")) {
 		boot_instance = env_get("boot_instance");
 		env_set("fastboot.boot_instance", boot_instance);
 	}
 	if (CONFIG_IS_ENABLED(OPTEE) &&
 	    tee_find_device(NULL, NULL, NULL, NULL)) {
-		if (CONFIG_IS_ENABLED(CONFIG_CMD_DTIMG))
+		if (CONFIG_IS_ENABLED(CMD_DTIMG))
 			env_set("android_bootargs", "androidboot.optee=true");
 		if (CONFIG_IS_ENABLED(FASTBOOT))
 			env_set("fastboot.boot_mode", "optee");
 	} else {
-		if (CONFIG_IS_ENABLED(CONFIG_CMD_DTIMG))
+		if (CONFIG_IS_ENABLED(CMD_DTIMG))
 			env_set("android_bootargs", "");
 		if (CONFIG_IS_ENABLED(FASTBOOT))
 			env_set("fastboot.boot_mode", "trusted");